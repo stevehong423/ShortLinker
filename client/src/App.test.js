5569c77cebb3a86fc348aef753516b5f7bd99368
Steve Hong <62493013+stevehong423@users.noreply.github.com>
2021-06-23 19:17:53 -0700

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { getUrls } from './api';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+//GET API Test
+describe('API Get Reqest', () => {
+  test('Responds with an array of objects', async () => {
+    const response = await getUrls()
+    expect(response[0].length).not.toBe(0)
+    expect(typeof response).toEqual('object')
+    expect(typeof response[0].id).toEqual('string')
+  })
+})
